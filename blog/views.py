babc6e767ee81349547b9c6330a50d838533316e
ATHARV <atharvtambe263@gmail.com>
2026-05-27 10:46:19 +0530

    search and blog feature

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Blog,category
+from django.db.models import Q
+
 
 def post_by_category(request, category_id):
     posts = Blog.objects.filter(status='Published', category=category_id)
@@ -10,3 +12,23 @@ def post_by_category(request, category_id):
         'category': category,
     }
     return render(request, 'post_by_category.html', context)
+
+def blog(request,slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    context = {
+        'single_blog' : single_blog,
+    }
+    return render(request , 'blog.html',context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    
+    blog = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published')
+  
+
+
+    context = {
+        'blog': blog,
+        'keyword': keyword,
+    }
+    return render(request,'search.html',context)
--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,9 +19,12 @@ from django.urls import include, path
 from . import views
 from  django.conf.urls.static import static
 from django.conf import settings
+from blog import views as Blogview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('category/',include('blog.urls')),
+    path('<slug:slug>',Blogview.blog,name='blog'),
+    path('blog/search/',Blogview.search,name='search')
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
