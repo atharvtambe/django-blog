6e64d4a536f2ad05704d209f3ac86434e6aba44e
ATHARV <atharvtambe263@gmail.com>
2026-05-28 10:29:33 +0530

    login and registration

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('category/',include('blog.urls')),
-    path('<slug:slug>',Blogview.blog,name='blog'),
-    path('blog/search/',Blogview.search,name='search')
+    path('blog/<slug:slug>',Blogview.blog,name='blog'),
+    path('blog/search/',Blogview.search,name='search'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
