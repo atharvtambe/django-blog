6276d3787e845b1c8e7fcd842fa42ab81670695f
ATHARV <atharvtambe263@gmail.com>
2026-05-30 19:11:55 +0530

    manage user functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import get_object_or_404, redirect, render
 from blog.models import category,Blog
 from django.contrib.auth.decorators import login_required
 
-from dashboards.forms import BlogPostForm, CategoryForm
+from dashboards.forms import AddUserForm, BlogPostForm, CategoryForm, EditUserForm
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 @login_required(login_url='login')
@@ -86,3 +87,42 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+def users(request):
+    users = User.objects.all()
+    context = {
+        'users': users,
+    }
+    return render(request, 'dashboards/users.html', context)
+
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = AddUserForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboards/add_user.html', context)
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = EditUserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+    form = EditUserForm(instance=user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboards/edit_user.html', context)
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
